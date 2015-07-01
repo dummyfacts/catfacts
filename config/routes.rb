8c50694b64a63d06d297109bed0144c647215de6
Hadi Badjian <hadi.badjian@outware.com.au>
2015-07-01 23:09:21 +1000

    disable IP whitelisting

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  scope :constraints => lambda{|req| req.remote_addr == ENV['WHITELISTED_IP'] } do
+  # scope :constraints => lambda{|req| req.remote_addr == ENV['WHITELISTED_IP'] } do
     resources :directories
     resources :facts
-  end
+  # end
 
   post 'sms' => 'sms#create'
 
